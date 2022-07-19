03c02cd0353ab7226360e8eb1797c92e063b3987
rickie maweu <rickiemaweu@gmail.com>
2022-07-20 00:38:38 +0300

    event handling lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+import React from "react";
+function focus() {
+    console.log('Good!')
+}
+function blur(){
+    console.log('Hey! Eyes on me!')
+}
+
+function EyesOnMe() {
+    return(
+        <button onFocus={focus} onBlur={blur}>Eyes on me </button>
+    )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,10 @@
-// Code Keypad Component Here
-
-function Keypad (){
-    return (
-        <div></div>
+import React from 'react'
+function Keypad() {
+    function change(){
+        console.log('Entering password...')
+    }
+    return(
+        <input onChange={change} type="password"/>
     )
 }
-
-export default Keypad;
+export default Keypad
